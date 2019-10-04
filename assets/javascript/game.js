11cda644c22cc66e756fa699afc493d146f76463
marie del borrello <53557430+marie1881@users.noreply.github.com>
2019-10-04 23:34:34 +0800

    update files

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var words = ['spider man', 'iron man', 'hulk', 'captain marvel', 'Thanos', 'Black Panther', 'Captain America', 'Black Widow', 'Hawkeye', 'Thor', 'Star Lord', 'Rocket', 'Groot'];
+
+var randNum = Math.floor(Math.random() * words.length);
+var choosenWord = words[randNum];
+console.log(choosenWord);
