a4d2efe0ea195e5b9b681ac1a77a3a557c7b605b
marie del borrello <53557430+marie1881@users.noreply.github.com>
2019-10-07 18:05:42 +0800

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,46 @@
-var words = ['spider man', 'iron man', 'hulk', 'captain marvel', 'Thanos', 'Black Panther', 'Captain America', 'Black Widow', 'Hawkeye', 'Thor', 'Star Lord', 'Rocket', 'Groot'];
+// Array of words - charcters from the marvel universe
+const words = ['spider man', 'iron man', 'hulk', 'captain marvel', 'Thanos', 'black panther', 'captain america', 'black widow', 'hawkeye', 'thor', 'star lord', 'rocket', 'groot'];
+// Word selected randomly
+let randNum = Math.floor(Math.random() * words.length);
+let choosenWord = words[randNum];
+let rightWord = [];
+let wrongWord = [];
+let underScore = [];
 
-var randNum = Math.floor(Math.random() * words.length);
-var choosenWord = words[randNum];
+let docUnderScore = document.getElementsByClassName('underscore');
+
 console.log(choosenWord);
+// Form underscores based on the word length - create for loop
+let generateUnderscore = () => {
+    for(let i = 0; i < choosenWord.length; i++) {
+        underScore.push('_');
+        docUnderScore[0].innerHTML = underScore.join(' '); 
+
+    }
+    return underScore;
+
+}
+
+console.log(generateUnderscore());
+//Users guess
+document.addEventListener('keypress', (event) => {
+    let keycode = event.keyCode;
+    let keyword = String.fromCharCode(keycode);
+//If guess is correct  
+    if(choosenWord.indexOf(keyword) > -1) {
+        rightWord.push(keyword);
+
+        underScore[choosenWord.indexOf(keyword)] = keyword;
+
+        if(underScore.join('') == choosenWord) {
+            alert('You Win');
+        }
+    }
+    else {
+        wrongWord.push(keyword);
+    }
+});
+
+generateUnderscore();
+
+//
