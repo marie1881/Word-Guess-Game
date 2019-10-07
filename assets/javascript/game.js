f73cd7cf724e54e448bce6e15513aaa336a0a699
marie del borrello <53557430+marie1881@users.noreply.github.com>
2019-10-07 20:27:31 +0800

    animation files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,45 @@
 // Array of words - charcters from the marvel universe
 const words = ['spider man', 'iron man', 'hulk', 'captain marvel', 'Thanos', 'black panther', 'captain america', 'black widow', 'hawkeye', 'thor', 'star lord', 'rocket', 'groot'];
 // Word selected randomly
-let randNum = Math.floor(Math.random() * words.length);
-let choosenWord = words[randNum];
-let rightWord = [];
-let wrongWord = [];
-let underScore = [];
+
+var randomWord = "";
+var lettersOfWord = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
+
+//Counter Variables
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 8;
+
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
+
+    // split the individual word into separate arrays, and store in new array 
+    lettersOfWord = randomWord.split("");
+
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
+}
+
+
+
 
 let docUnderScore = document.getElementsByClassName('underscore');
 
